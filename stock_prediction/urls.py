6e6e10c604dc30aa4bcede92764d524edd1e1df1
Michael Kiros <michaelakiros@gmail.com>
2019-10-21 17:37:24 -0500

    Add hello world for homepage URL (i.e. /)
    This returns a "Hello, world." message when you open the homepage.

--- stock_prediction/urls.py
+++ stock_prediction/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, world.")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
 ]
