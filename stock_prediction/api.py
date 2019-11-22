1f286979ddc944c5165c4e8812bd49f33b5baedc
Michael Kiros <michaelakiros@gmail.com>
2019-11-22 03:44:20 -0600

    Merge views branch into login
    The pages are now working, the designs are there, but I just found out
    that AlphaVantage rate limits to 5 calls per minute. Sigh. We need a
    better API if this is going to work.

--- stock_prediction/api.py
+++ stock_prediction/api.py
@@ -10,6 +10,7 @@ import requests
 
 API_KEY = '5IPQ89L576QGG667'
 
+
 class StockQuote:
 	"""
 	Encapsulates a single stock quote for a given stock on
@@ -20,6 +21,7 @@ class StockQuote:
 		self.symbol = symbol
 		self.date = date
 		self.price = price
+
 
 def get_stock_history(symbol) -> List[StockQuote]:
 	"""
@@ -47,6 +49,7 @@ def get_stock_history(symbol) -> List[StockQuote]:
 
 	return stock_quotes
 
+
 def get_stock_quote(symbol) -> StockQuote:
 	"""
 	Returns the latest stock quote of a stock. Note that the stock price
@@ -54,5 +57,50 @@ def get_stock_quote(symbol) -> StockQuote:
 	the previous day.
 	TODO: Consider using a different API call to speed it up.
 	"""
-	quotes = get_stock_history(symbol)
-	return quotes[0]
+	print('finding: ' + symbol)
+	response = requests.get('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={}&apikey={}'.format(
+		symbol,
+		API_KEY
+	))
+
+	try:
+		print(response.text)
+		data_key = 'Global Quote'
+		data = dict(json.loads(response.text))[data_key]
+	except:
+		raise Exception('Bad symbol given') # TODO: Bad idea
+
+	date = data['07. latest trading day'] # TODO: Improve date formatting
+	price = data['05. price']
+	return StockQuote(symbol, date, price)
+
+
+def search_stock_quote(keywords) -> List[StockQuote]:
+	"""
+	Searches for the specified text, keywords using the AlphaVantage API and
+	returns the quotes for any stock quotes that match the text (even partially).
+
+	Returns an empty List if nothing is found.
+	"""
+	response = requests.get('https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={}&apikey={}'.format(
+		keywords,
+		API_KEY
+	))
+
+	try:
+		data_key = 'bestMatches'
+		data = dict(json.loads(response.text))[data_key]
+	except:
+		return []
+
+	results = []
+	for r in data:
+		# TODO: For each quote, get the information
+		try:
+			quote = get_stock_quote(r['1. symbol'])
+		except:
+			# Probably AlphaVantage rate limiting
+			continue
+		results.append(quote)
+
+	return results
--- stock_prediction/urls.py
+++ stock_prediction/urls.py
@@ -13,25 +13,75 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import datetime
+
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from . import api
+
 # def index(request):
 #     return HttpResponse("andi oop.")
 
+
+class stocks:  
+    def __init__(stock, name, company, price, change, arrow):
+        stock.name = name  
+        stock.company = company
+        stock.price = price
+        stock.change = change
+        stock.arrow = arrow
+   
+# creating list        
+
+def get_current_date() -> str:
+    today = datetime.date.today()
+    return '{} {} {}, {}'.format(
+        today.strftime('%A'),
+        today.strftime('%B'),
+        today.strftime('%d'),
+        today.strftime('%Y')
+    )
+
 def dashboard(request):
-    return render(request, 'stock_prediction/dashboard.html', {})
+    return render(request, 'stock_prediction/dashboard.html', {
+        'date': get_current_date()
+    })
 
 def saved(request):
-    return render(request, 'stock_prediction/saved.html', {})
+    stocklist = []  
+    stocklist.append( stocks('Saved Stock 1', 'Company', 0, 'price increase', 'increase') ) 
+    stocklist.append( stocks('Saved Stock 2', 'Company', 0, 'price increase', 'increase') ) 
+    stocklist.append( stocks('Saved Stock 3', 'Company', 0, 'price decrease', 'decrease') )
 
-def search(request):
-    return render(request, 'stock_prediction/search.html', {})
+    context= {
+        'stocks': stocklist,
+        }
+    return render(request, 'stock_prediction/saved.html', context)
+
+def search(request, keywords=None):
+    results = api.search_stock_quote(keywords)
+
+    stocklist = []
+    for r in results:
+        stocklist.append(stocks(r.symbol, '<COMPANY>', r.price, '<DIFFERENCE>', 'increase'))
+
+    context= {
+        'stocks': stocklist,
+    }
+
+    return render(request, 'stock_prediction/search.html', context)
 
 def details(request):
-    return render(request, 'stock_prediction/details.html', {})
+    stockdetail = stocks('NASDAQ', 'NASDAQ Composite', 7950.86, '+48.67', 'increase') 
+    context= {
+        'stock': stockdetail,
+        }
+    return render(request, 'stock_prediction/details.html', context)
+
+
 
 
 
@@ -42,10 +92,12 @@ def details(request):
 
 
 urlpatterns = [
+    path('', dashboard),
     path('admin/', admin.site.urls),
     path('dashboard/', dashboard),
     path('saved/', saved),
     path('search/', search),
+    path('search/<keywords>/', search),
     path('details/', details),
 ]
 
